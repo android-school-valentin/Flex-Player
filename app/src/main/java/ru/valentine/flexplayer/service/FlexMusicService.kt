cfef7b4416f2b891c32b0be7bf3aea5347a5a283
Valentin <valentin7petr@gmail.com>
2021-12-20 20:32:33 +0300

    Added normal notification handling

--- app/src/main/java/ru/valentine/flexplayer/service/FlexMusicService.kt
+++ app/src/main/java/ru/valentine/flexplayer/service/FlexMusicService.kt
@@ -1,22 +1,25 @@
 package ru.valentine.flexplayer.service
 
-import android.content.pm.ServiceInfo
-import android.os.Build
+import android.app.Notification
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_BROWSABLE
 import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
 import android.support.v4.media.MediaDescriptionCompat
-import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
-import android.support.v4.media.session.PlaybackStateCompat
-import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.DefaultControlDispatcher
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import ru.valentine.flexplayer.R
 import ru.valentine.flexplayer.core.media.MalformedMediaIdException
 import ru.valentine.flexplayer.core.media.parse
 import ru.valentine.flexplayer.service.browser.BrowserTree
@@ -25,66 +28,99 @@ import ru.valentine.flexplayer.service.browser.tree.MediaCategory
 import ru.valentine.flexplayer.service.browser.tree.MediaContent
 import ru.valentine.flexplayer.service.ext.EXTRA_DURATION
 import ru.valentine.flexplayer.service.ext.EXTRA_NUMBER_OF_TRACKS
-import ru.valentine.flexplayer.service.playback.FlexPlaybackPreparer
-import ru.valentine.flexplayer.service.playback.FlexQueueNavigator
-import ru.valentine.flexplayer.service.playback.NOW_PLAYING_NOTIFICATION
-import ru.valentine.flexplayer.service.playback.NotificationBuilder
+import ru.valentine.flexplayer.service.playback.*
 import timber.log.Timber
+import kotlin.coroutines.CoroutineContext
 
-class FlexMusicService : BaseMusicService() {
+const val NOW_PLAYING_CHANNEL_ID = "ru.valentine.flexplayer.media.NOW_PLAYING"
+const val NOW_PLAYING_NOTIFICATION_ID = 0xb339 // Arbitrary number used to identify our notification
+
+class FlexMusicService : MediaBrowserServiceCompat(), CoroutineScope {
+
+    private lateinit var scopeJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = scopeJob + Dispatchers.Main.immediate
 
     private val browserTree: BrowserTree by inject()
     private val session: MediaSessionCompat by inject()
     private val player: ExoPlayer by inject()
-    private val notificationBuilder: NotificationBuilder by inject()
+    private val playerListener = PlayerEventListener()
 
     private lateinit var mediaController: MediaControllerCompat
-    private lateinit var notificationManager: NotificationManagerCompat
+    private lateinit var notificationManager: PlayerNotificationManager
 
-    private val controllerCallback = MediaControllerCallback()
+    private var isForegroundService = false
 
     override fun onCreate() {
         super.onCreate()
+        scopeJob = SupervisorJob()
 
+        session.isActive = true
+
+        player.addListener(playerListener)
 
         mediaController = MediaControllerCompat(this, session)
 
-        mediaController.registerCallback(controllerCallback)
-
-
         val mediaSessionConnector = MediaSessionConnector(session)
-
         mediaSessionConnector.setPlayer(player)
-
         mediaSessionConnector.setPlaybackPreparer(
-                FlexPlaybackPreparer(
-                        scope = this,
-                        player = player,
-                        browserTree = browserTree
-                )
+            FlexPlaybackPreparer(
+                scope = this,
+                player = player,
+                browserTree = browserTree
+            )
         )
         mediaSessionConnector.setQueueNavigator(FlexQueueNavigator(session = session))
 
-        notificationManager = NotificationManagerCompat.from(this)
+        notificationManager = PlayerNotificationManager.createWithNotificationChannel(
+            this,
+            NOW_PLAYING_CHANNEL_ID,
+            R.string.channel_mediasession,
+            R.string.channel_mediasession_description,
+            NOW_PLAYING_NOTIFICATION_ID,
+            DescriptionAdapter(this, this, mediaController),
+            FlexNotificationListener()
+        ).apply {
+
+            setMediaSessionToken(session.sessionToken)
+            setSmallIcon(R.drawable.ic_music_note)
+            setControlDispatcher(DefaultControlDispatcher(0, 0))
+
+        }
+        notificationManager.setPlayer(player)
+
+        //notificationManager = NotificationManagerCompat.from(this)
 
         sessionToken = session.sessionToken
 
     }
 
     override fun onDestroy() {
-        session.release()
+        session.run {
+            isActive = false
+            release()
+        }
+
+        scopeJob.cancel()
+
+        notificationManager.setPlayer(null)
+        player.removeListener(playerListener)
+        player.release()
         super.onDestroy()
     }
 
     override fun onGetRoot(
-            clientPackageName: String,
-            clientUid: Int,
-            rootHints: Bundle?
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
     ): BrowserRoot {
         return BrowserRoot("root", null)
     }
 
-    override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
         result.detach()
 
         launch {
@@ -107,9 +143,9 @@ class FlexMusicService : BaseMusicService() {
 
                 } catch (malformedId: MalformedMediaIdException) {
                     Timber.i(
-                            malformedId,
-                            "Attempt to load item from a malformed media id: %s",
-                            itemId
+                        malformedId,
+                        "Attempt to load item from a malformed media id: %s",
+                        itemId
                     )
                     result.sendResult(null)
                 }
@@ -119,28 +155,28 @@ class FlexMusicService : BaseMusicService() {
 
 
     private fun MediaContent.toItem(
-            builder: MediaDescriptionCompat.Builder = MediaDescriptionCompat.Builder()
+        builder: MediaDescriptionCompat.Builder = MediaDescriptionCompat.Builder()
     ): MediaBrowserCompat.MediaItem {
         builder
-                .setMediaId(id.encoded)
-                .setTitle(title)
-                .setIconUri(iconUri)
+            .setMediaId(id.encoded)
+            .setTitle(title)
+            .setIconUri(iconUri)
 
         when (this) {
             is MediaCategory -> {
                 builder
-                        .setSubtitle(subtitle)
-                        .setExtras(Bundle().apply {
-                            putInt(EXTRA_NUMBER_OF_TRACKS, count)
-                        })
+                    .setSubtitle(subtitle)
+                    .setExtras(Bundle().apply {
+                        putInt(EXTRA_NUMBER_OF_TRACKS, count)
+                    })
             }
 
             is AudioTrack -> {
                 builder
-                        .setSubtitle(artist)
-                        .setExtras(Bundle(1).apply {
-                            putLong(EXTRA_DURATION, duration)
-                        })
+                    .setSubtitle(artist)
+                    .setExtras(Bundle(1).apply {
+                        putLong(EXTRA_DURATION, duration)
+                    })
             }
         }
 
@@ -155,104 +191,47 @@ class FlexMusicService : BaseMusicService() {
         return MediaBrowserCompat.MediaItem(builder.build(), flags)
     }
 
-    private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
-
-        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
-            mediaController.playbackState?.let(this::updateServiceState)
-        }
-
-        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-            state?.let(this::updateServiceState)
-        }
-
-        override fun onShuffleModeChanged(shuffleMode: Int) {
-            //settings.shuffleModeEnabled = shuffleMode == PlaybackStateCompat.SHUFFLE_MODE_ALL
-        }
-
-        override fun onRepeatModeChanged(repeatMode: Int) {
-            //settings.repeatMode = when (repeatMode) {
-            //    PlaybackStateCompat.REPEAT_MODE_ALL,
-            //    PlaybackStateCompat.REPEAT_MODE_GROUP -> RepeatMode.ALL
-            //    PlaybackStateCompat.REPEAT_MODE_ONE -> RepeatMode.ONE
-            //    else -> RepeatMode.DISABLED
-            //}
-        }
-
-        private fun updateServiceState(state: PlaybackStateCompat) {
-            val updatedState = state.state
-            if (mediaController.metadata == null) {
-                // Do not update service when no metadata.
-                return
-            }
-
-            when (updatedState) {
-                // Playback started or has been resumed.
-                PlaybackStateCompat.STATE_PLAYING -> onPlaybackStarted()
-
-                // Playback has been paused.
-                PlaybackStateCompat.STATE_PAUSED -> onPlaybackPaused()
-
-                // Playback ended or an error occurred.
-                PlaybackStateCompat.STATE_NONE,
-                PlaybackStateCompat.STATE_STOPPED,
-                PlaybackStateCompat.STATE_ERROR -> onPlaybackStopped()
-
-                else -> {
-                    // Intentionally empty.
-                }
-            }
-        }
-
-        private fun onPlaybackStarted() {
-            // Activate the media session if not active
-            if (!session.isActive) {
-                session.isActive = true
-            }
-
-            // Display a notification, putting the service to the foreground.
-            val notification = notificationBuilder.buildNotification()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                startForeground(
-                        NOW_PLAYING_NOTIFICATION,
-                        notification,
-                        ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+    private inner class FlexNotificationListener :
+        PlayerNotificationManager.NotificationListener {
+        override fun onNotificationPosted(
+            notificationId: Int,
+            notification: Notification,
+            ongoing: Boolean
+        ) {
+            if (ongoing && !isForegroundService) {
+                ContextCompat.startForegroundService(
+                    applicationContext,
+                    Intent(applicationContext, this@FlexMusicService.javaClass)
                 )
-            } else {
-                startForeground(NOW_PLAYING_NOTIFICATION, notification)
-            }
 
-            // Start the service to keep it playing even when all clients unbound.
-            this@FlexMusicService.startSelf()
-        }
-
-        private fun onPlaybackPaused() {
-            // Put the service back to the background, keeping the notification
-            stopForeground(false)
-            //settings.lastPlayedPosition = player.currentPosition
-
-            // Update the notification content if the session is active
-            if (session.isActive) {
-                notificationManager.notify(NOW_PLAYING_NOTIFICATION, notificationBuilder.buildNotification())
+                startForeground(notificationId, notification)
+                isForegroundService = true
             }
         }
 
-        private fun onPlaybackStopped() {
-            // Clear notification and service foreground status
+        override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
             stopForeground(true)
-
-            // De-activate the media session.
-            if (session.isActive) {
-                session.isActive = false
-            }
-
-            // Stop the service, killing it if it is not bound.
-            this@FlexMusicService.stop()
+            isForegroundService = false
+            stopSelf()
         }
+    }
 
-        override fun onSessionDestroyed() {
-            player.run {
-                stop()
-                release()
+    private inner class PlayerEventListener : Player.EventListener {
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            when (playbackState) {
+                Player.STATE_BUFFERING,
+                Player.STATE_READY -> {
+                    notificationManager.setPlayer(player)
+                    if (playbackState == Player.STATE_READY) {
+                        if (!playWhenReady) {
+                            stopForeground(false)
+                            isForegroundService = false
+                        }
+                    }
+                }
+                else -> {
+                    notificationManager.setPlayer(null)
+                }
             }
         }
     }
